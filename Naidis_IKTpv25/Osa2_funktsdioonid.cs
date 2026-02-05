851f155d169e34edec1dbc21d4d979aaa8b102dd
TsalkoIlja <iljatsalko9@gmail.com>
2026-02-05 18:40:20 +0200

    Refactor class name and improve ticket logic

--- Naidis_IKTpv25/Osa2_funktsdioonid.cs
+++ Naidis_IKTpv25/Osa2_funktsdioonid.cs
@@ -4,45 +4,58 @@ using System.Text;
 
 namespace Naidis_IKTpv25
 {
-    public class Osa2_funktsdioonid
+    public class Osa2_funktsioonid
     {
+        
         public static string Juku(string nimi="Juku")
         {
             string otsus = "";
             string vastus = "";
             int vanus = 0;
             string pilet = "";
-            Console.WriteLine("Mis on sinu nimi?       ");
-            string nimi = Console.ReadLine();
-            if (nimi.ToLower() =="juku")
+            Console.WriteLine(nimi);
+            if (nimi.ToLower() == "juku")
             {
-                Console.Write("Kui vana sa oled?:");
+                Console.Write("Kui vana sa oled?: ");
                 try
                 {
                     vanus = int.Parse(Console.ReadLine());
                     if (vanus > 0 && vanus<100)
                     {
-                        if (vanus<6)
+                        if (vanus < 6)
                         {
-                            pilet = "Sulle on kinopilet tasuta"
+                            pilet="Sulle on kinopilet tasuta!";
                         }
-                        pilet = "Sulle on kinopilet on lastepilet! ";
+                        else if (vanus >= 6 && vanus < 14)
+                        {
+                            pilet="Sulle on kinopilet on lastepilet!";
+                        }
+                        else if (vanus >= 14 && vanus < 65)
+                        {
+                            pilet="Sulle on kinopilet täispilet!";
+                        }
+                        else if (vanus >= 65)
+                        {
+                            pilet="Sulle on kinopilet sooduspilet!";
+                        }
+                    }
+                    else 
+                    { 
+                        pilet="Tundmatupilet. Vanus peab olema >0 arv ja <100!";
                     }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine
+                    Console.WriteLine(e);
                 }
-
-                otsus = "Lähme kinno! {pilet}";
+                otsus= $"Lähme kinno! {pilet}";
             }
-
             else
             {
-                otsus = "Ma olen võivatud";
-            }
-            vastus = $"Tere {nimi}, {otsus}";
-                return vastus;
-        }
+                otsus = "Ma olen hõivatud" ;
+            } 
+            vastus=$"Tere {nimi}, {otsus}";
+            return vastus; 
+        }   
     }
 }
