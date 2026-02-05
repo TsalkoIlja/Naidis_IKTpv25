e8f754af6600f5143b09289a1364bf1e81adf654
TsalkoIlja <iljatsalko9@gmail.com>
2026-02-05 14:23:54 +0200

    Add project files.

---
+++ Naidis_IKTpv25/Osa2_funktsdioonid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naidis_IKTpv25
+{
+    public class Osa2_funktsdioonid
+    {
+        public static string Juku(string nimi="Juku")
+        {
+            string otsus = "";
+            string vastus = "";
+            int vanus = 0;
+            string pilet = "";
+            Console.WriteLine("Mis on sinu nimi?       ");
+            string nimi = Console.ReadLine();
+            if (nimi.ToLower() =="juku")
+            {
+                Console.Write("Kui vana sa oled?:");
+                try
+                {
+                    vanus = int.Parse(Console.ReadLine());
+                    if (vanus > 0 && vanus<100)
+                    {
+                        if (vanus<6)
+                        {
+                            pilet = "Sulle on kinopilet tasuta"
+                        }
+                        pilet = "Sulle on kinopilet on lastepilet! ";
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine
+                }
+
+                otsus = "Lähme kinno! {pilet}";
+            }
+
+            else
+            {
+                otsus = "Ma olen võivatud";
+            }
+            vastus = $"Tere {nimi}, {otsus}";
+                return vastus;
+        }
+    }
+}
---
+++ Naidis_IKTpv25/StartPage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Naidis_IKTpv25
+{
+    public class StartPage
+    {
+        public static void Main(string [] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("1. Osa Andmetüüpid, Alamfunktsioonid/meetodid");
+            string tekst = "Tere tulemast C3-i maailma!";
+            Console.WriteLine($"Oli loodud muutuja tekst, mis võrdub: {tekst}");
+            Console.WriteLine("Oli loodud muutuja tekst, mis võrdub: {0}", tekst);
+            //Console.Write("Mis on sinu nimi?");
+
+            Random rnd = new Random();
+            int juhuslik_arv = rnd.Next(-5, 25);
+            Console.WriteLine(Naidis_funktsioonid.Kuu_nimetus(juhuslik_arv));
+            
+           
+            //try
+            //{
+            //    int vanus = int.Parse(Console.ReadLine());
+            //    Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana");
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //}
+            //Console.Write("Arv 1: ");
+            //float a = float.Parse(Console.ReadLine());
+            //Console.Write("Arv 2:");
+            //float b = float.Parse(Console.ReadLine());
+            //float vastus = Naidis_funktsioonid.Summa(a, b);
+            //Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
+
+            //try
+            //{
+
+            //}
+             
+            //catch (Exception e)
+
+            //{ 
+            //Console.WriteLine(e);
+            //}
+
+
+        }
+    }
+}
